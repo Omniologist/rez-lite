7d135c85b1371093d0191ad7b3d6de8b5f7ceab9
omniologist <sjtciu@gmail.com>
2026-05-13 15:27:44 +0930

    feat: version compare command implemented

--- __main__.py
+++ __main__.py
@@ -9,10 +9,25 @@ def main():
         case "parse":
             print(f"version({args[0]}) => tokens: {parse(args[0])}")
 
+        case "compare":
+            print(f"{args[0]} {compare(args[0], args[1])} {args[1]}")
 
-def parse(raw: str):
-    print(1)
-    return [int(c) for c in raw.split(".")]
+
+def parse(version: str):
+    tokens = [int(c) for c in version.split(".")]
+    return tokens
+
+
+# modify to use enums
+def compare(a, b):
+    a_tokenized = parse(a)
+    b_tokenized = parse(b)
+    if a_tokenized < b_tokenized:
+        return "<"
+    elif a_tokenized > b_tokenized:
+        return ">"
+    elif a_tokenized == b_tokenized:
+        return "=="
 
 
 main()
